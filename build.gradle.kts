f49752db06011826026cac87941521e5c4f863eb
Shibler0 <138115345+Shibler0@users.noreply.github.com>
2025-01-12 21:51:52 +0100

    previous not working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    `maven-publish`
+    id("maven-publish")
 }
 
 android {
@@ -50,7 +50,6 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-
         }
     }
 }
