f8baa0a9483a6436b4885b12e0a13400310270b8
Shibler0 <138115345+Shibler0@users.noreply.github.com>
2025-01-18 19:15:55 +0100

    previous not workings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
+    id("maven-publish")
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
 }
 
 android {
@@ -31,19 +31,19 @@ android {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.12.01")) // Remplacez par la version souhaitée
-    implementation("androidx.compose.ui:ui:1.7.6") // Remplacez par la version souhaitée
-    implementation("androidx.compose.ui:ui-graphics:1.7.6") // Remplacez par la version souhaitée
-    implementation("androidx.compose.material3:material3:1.3.1") // Remplacez par la version souhaitée
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-graphics:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 }
 
